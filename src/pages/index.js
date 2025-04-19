23c42773bbe824089248a323e87c843102687a21
AbdulRafay54 <rafayabdul7817@gmail.com>
2025-04-19 13:11:05 +0500

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import Products from "./Products/page";
 import Contact from "./Contact/page";
 import Footer from "./Footer/page";
 import Auth from "./Auth/page";
+import Swal from "sweetalert2";
 
 const Index = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -23,8 +24,24 @@ const Index = () => {
   };
 
   const handleLogout = () => {
-    localStorage.removeItem("loggedInUser");
-    setIsLoggedIn(false);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You are about to logout!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, logout!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        localStorage.removeItem("loggedInUser");
+
+        setIsLoggedIn(false);
+
+        // Show a success alert after logout
+        Swal.fire("Logged out!", "You have been logged out.", "success");
+      }
+    });
   };
 
   if (!isLoggedIn) {
@@ -40,7 +57,10 @@ const Index = () => {
       <Contact />
       <Footer />
       <div className="fixed bottom-4 right-4">
-        <button class="group flex items-center justify-start w-11 h-11 bg-gradient-to-r from-red-700 to-pink-400 rounded-full cursor-pointer relative overflow-hidden transition-all duration-300 shadow-lg hover:w-32 hover:rounded-lg active:translate-x-1 active:translate-y-1">
+        <button
+          class="group flex items-center justify-start w-11 h-11 bg-gradient-to-r from-red-700 to-pink-400 rounded-full cursor-pointer relative overflow-hidden transition-all duration-300 shadow-lg hover:w-32 hover:rounded-lg active:translate-x-1 active:translate-y-1"
+          onClick={handleLogout}
+        >
           <div class="flex items-center justify-center w-full transition-all duration-300 group-hover:justify-start group-hover:px-3">
             <svg class="w-4 h-4" viewBox="0 0 512 512" fill="white">
               <path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"></path>
